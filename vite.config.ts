4378cfbb135afe39f55619fa3c05bf25e5d376f9
nirajaj <nirajaj133@gmail.com>
2025-07-22 19:34:02 +0545

    Update all the file for more better ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 VERY IMPORTANT for GitHub Pages + Custom Domain
+export default defineConfig({
+  base: "/", // ✅ for GitHub Pages with custom domain
   server: {
     host: "localhost",
     port: 5173,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
